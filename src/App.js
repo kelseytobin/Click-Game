24e42f6af1ef8ce9285869add2336356c214da2a
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-09 10:48:02 -0600

    created base navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import HelloTest from "./components/HelloTest";
+import NavBar from "./components/NavBar/Navbar";
 
 function App() {
-  return <HelloTest />;
+  return <NavBar />;
 }
 
 
---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div class="navbar-fixed">
+            <nav>
+                <div class="nav-wrapper">
+                <a href="#!" class="brand-logo left">Click an image to begin!</a>
+                <a href="#!" class="brand-logo center">Try to click all 18 images to win!</a>
+                <a href="#!" class="brand-logo right">Be careful not to click the same one twice!</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
