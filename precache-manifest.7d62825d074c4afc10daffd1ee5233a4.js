f3e501495edb56552936f3e15fc32976d8e06da6
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-11 10:57:52 -0600

    Updates

--- precache-manifest.7d62825d074c4afc10daffd1ee5233a4.js
+++ precache-manifest.7d62825d074c4afc10daffd1ee5233a4.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/Click-Game/static/js/runtime~main.bf617116.js"
   },
   {
-    "revision": "0cfa446a8fb7fe2524b7",
-    "url": "/Click-Game/static/js/main.8039b622.chunk.js"
+    "revision": "1097d07de4b404e6e62b",
+    "url": "/Click-Game/static/js/main.5e6a9fd7.chunk.js"
   },
   {
     "revision": "45124f853ba07aa965c0",
     "url": "/Click-Game/static/js/2.6efc73d3.chunk.js"
   },
   {
-    "revision": "0cfa446a8fb7fe2524b7",
-    "url": "/Click-Game/static/css/main.bbb37044.chunk.css"
+    "revision": "1097d07de4b404e6e62b",
+    "url": "/Click-Game/static/css/main.c61b3b41.chunk.css"
   },
   {
-    "revision": "3d33fc9a9fb9398569148bf3281d7d57",
+    "revision": "9273c73b3fc85d9654d5e7282eedf491",
     "url": "/Click-Game/index.html"
   }
 ];
