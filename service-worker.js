a9b8525f008b5235926e179bc29982ecf1507c4b
Kelsey Tobin <etobinkelsey@gmail.com>
2019-04-10 20:54:11 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Click-Game/precache-manifest.49fce93d8715ce5d46bc91599e8cd84f.js"
+  "/Click-Game/precache-manifest.fbdbdb7dbc037c5ba4331ebe882a2213.js"
 );
 
 workbox.clientsClaim();
